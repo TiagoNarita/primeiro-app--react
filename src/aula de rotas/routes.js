d49bc43b39628605b5bff029abc252d2e2ee50eb
Tiago <tiagobnarita@gmail.com>
2024-03-12 23:00:10 -0300

    class number 03/12

---
+++ src/aula de rotas/routes.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/index";
+import Sobre from "./pages/Sobre/index";
+
+function RoutesApp() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default RoutesApp;
