81b27e4c30094123a431269a9d72e22989f4285c
Tiago <tiagobnarita@gmail.com>
2024-03-12 23:16:18 -0300

    routes just this

--- src/aula de rotas/routes.js
+++ src/aula de rotas/routes.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index";
 import Sobre from "./pages/Sobre/index";
+import Header from "./Header";
 
 function RoutesApp() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
-                <Route />
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<Sobre />} />
             </Routes>
         </BrowserRouter>
     );
